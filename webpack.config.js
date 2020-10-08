c1874e3bba799096ae797bc2808298fafe8b49bb
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-10-09 02:19:53 +0300

    init: create project

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import * as fs from "fs";
+
+const PATHS = require('path');
+require('html-webpack-plugin');
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
+
+module.exports = {
+  entry: {
+    main: './shelter/src/pages/main/main.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: PATHS.resolve('./shelter', './dist'),
+  },
+  
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    ...PAGES.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/${page}`,
+      filename: `./${page.replace(/\.pug/,'.html')}`
+    }))
+  ],
+  
+};
